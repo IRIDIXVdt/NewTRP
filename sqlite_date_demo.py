7bcf3f23596b1ff20a951f20b670a6042f98b638
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2021-06-09 19:06:55 +0800

    time method revised
    we can get the so called "today's list"

--- sqlite_date_demo.py
+++ sqlite_date_demo.py
@@ -1,4 +1,5 @@
 import sqlite3
+import math
 conn = sqlite3.connect(":memory:")
 c = conn.cursor()
 
@@ -7,16 +8,37 @@ def get_date_from_now(diff = 0):
     # we calculate the new date for a term. 
     # we intend to update it to the database right after the use of this method
     d = conn.cursor()
-    date_modifier = "+" + str(diff) + " days"
+
+    # date_modifier = "+" + str(diff) + " days"
+
+    if(diff>=0):
+        date_modifier = "+" + str(diff) + " days"
+    else:
+        date_modifier = str(diff) + " days"
+
     # print(date_modifier) the modifer would be, for instance '+1 day'
     d.execute("SELECT DATE(?,?)",('now',date_modifier))
     text1 = d.fetchone()[0]
     return text1
 
-def insert_term(textself,reading,levelTerm,levelReading):
+def insert_term(textself,reading,levelTerm):
     nextDate = get_date_from_now(1)
-    print(nextDate)
-    c.execute("INSERT INTO Term(termSelf,reading,levelTerm,levelReading,nextDate) VALUES (?,?,?,?,?)",(textself,reading,levelTerm,levelReading,nextDate))
+    c.execute("INSERT INTO Term(termSelf,reading,levelTerm,nextDate) VALUES (?,?,?,?)",(textself,reading,levelTerm,nextDate))
+
+def get_date_diff(day_term,day_target):
+    d = conn.cursor()
+    print(day_term,day_target)
+    d.execute("SELECT julianday(?) - julianday(?)",(day_term,day_target))
+    day = math.floor(d.fetchone()[0])
+    return day
+
+def get_today_list():
+    d = conn.cursor()
+    # d.execute(""" SELECT * from Term where julianday('now') >= julianday(nextDate); """)
+    
+    d.execute(""" SELECT * from Term where Date('now') >= nextDate; """)
+    return d.fetchall()
+
 
 c.execute("""
     CREATE TABLE Term(
@@ -24,22 +46,40 @@ c.execute("""
         termSelf TEXT,
         reading TEXT,
         levelTerm INTEGER,
-        levelReading INTEGER,
         nextDate TEXT
-    )""")
+    )
+    CREATE TABLE Definition(
+    termId INTEGER,
+    definition TEXT
+    )
+    CREATE TABLE SampleSentence(
+    termId INTEGER,
+    sentence TEXT,
+    translation TEXT
+    )
+""")
 
-insert_term('向こう','むこう',1,1)
-insert_term('晴れ','はれ',1,1)
+insert_term('向こう','むこう',1)
+c.execute("INSERT INTO Term(termSelf,reading,levelTerm,nextDate) VALUES (?,?,?,?)",('廊下','ろうか',1,get_date_from_now(-1)))
+c.execute("INSERT INTO Term(termSelf,reading,levelTerm,nextDate) VALUES (?,?,?,?)",('廊下','ろうか',1,get_date_from_now(0)))
+c.execute("INSERT INTO Term(termSelf,reading,levelTerm,nextDate) VALUES (?,?,?,?)",('廊下','ろうか',1,get_date_from_now(2)))
+# insert_term('晴れ','はれ',1)
 
-c.execute("""
-    SELECT * from term
-    """)
+c.execute("""SELECT * from Term""")
 print(c.fetchall())
+
+# test date difference calculations
+# day_diff = get_date_diff(get_date_from_now(1),get_date_from_now(5))
+# print(day_diff)
 
 # c.execute("""
 #     SELECT DATE('now');
 #     """)
 # print(c.fetchall())
+
+# now we test get today term
+list_demo = get_today_list()
+print(list_demo)
 
 conn.commit()
 conn.close()
