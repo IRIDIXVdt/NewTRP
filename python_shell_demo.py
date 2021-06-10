56e3721376693e01b6d2f96350f7fcb219bb0c8c
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2021-06-10 10:17:49 +0800

    prompting questions

---
+++ python_shell_demo.py
@@ -0,0 +1,39 @@
+import os
+clear = lambda: os.system('cls') #on Windows System
+
+print('for help, type in h')
+
+def insert_term():
+    while 1:
+        term = input("The term is: ")
+        reading = input("The reading of the term is: ")
+        
+while 1:
+    x = input(">>> ")
+    if x == 'exit':
+        break
+    elif x == 'h':
+        print("""
+        ====================================
+        * to insert, type "insert"
+        * to start with todays' list, type "start"
+        * to add sample sentences, type "sample"
+        * to add definitions, type "defi"
+        * to clear the console, type "clear"
+        * to exit, type "exit"
+        ====================================
+        """)
+    elif x == 'insert':
+        exit
+    elif x == 'start':
+        exit
+    elif x == 'sample':
+        exit
+    elif x == 'defi':
+        exit
+    elif x == 'clear':
+        clear()
+    elif x == 'print list':
+        print('test code*')
+    
+    
